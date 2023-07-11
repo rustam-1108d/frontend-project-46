15e764816204e6552cb8f935c03c025ed87edb49
rustam-1108d <133682982+rustam-1108d@users.noreply.github.com>
2023-07-12 00:12:45 +0600

    Add help option using commander.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,3 +1,12 @@
 #!/usr/bin/env node
+import { Command } from "commander";
 
-console.log('Hello, world!');
+const program = new Command();
+
+program
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .version('0.1.0')
+  .helpOption('-h, --help', 'output usage information');
+
+program.parse();
