d4fe905319c4d11fa3925f7f8d0bc6d1f47d94a2
rustam-1108d <133682982+rustam-1108d@users.noreply.github.com>
2023-08-04 15:07:03 +0600

    Add <filepath> arguments in bin/gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,6 +7,7 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('0.1.0')
-  .helpOption('-h, --help', 'output usage information');
+  .helpOption('-h, --help', 'output usage information')
+  .arguments('<filepath1> <filepath2>');
 
 program.parse();
