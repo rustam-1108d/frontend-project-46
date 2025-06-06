8622d25cc90221a4d78ce721100c4694e7b9415e
rustam-1108d <133682982+rustam-1108d@users.noreply.github.com>
2025-06-07 04:04:16 +0500

    Add tests for plain json files

---
+++ __tests__/genDiff.test.js
@@ -0,0 +1,19 @@
+import { fileURLToPath } from 'url';
+import path from 'path';
+import { readFileSync } from 'fs';
+import genDiff from '../src/index.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFixture = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
+
+test('gendiff flat json files (stylish format)', () => {
+  const file1 = getFixturePath('file1.json');
+  const file2 = getFixturePath('file2.json');
+  const expected = readFixture('expected-stylish.txt').trim();
+
+  const result = genDiff(file1, file2);
+  expect(result).toBe(expected);
+});
