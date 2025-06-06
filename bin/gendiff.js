9be736572a3f1027b2b60ad4931dacc23bd7e383
rustam-1108d <133682982+rustam-1108d@users.noreply.github.com>
2025-06-06 22:03:47 +0500

    Fix bin/gendiff.js --help option information

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,7 +7,6 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('0.1.0')
-  .helpOption('-h, --help', 'output usage information')
   .option('-f, --format <type>', 'output format')
   .arguments('<filepath1> <filepath2>');
 
