43a991ade42d88ddd66777c788b099646f248564
rustam-1108d <133682982+rustam-1108d@users.noreply.github.com>
2025-06-09 06:08:23 +0500

    Update version number

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,7 +7,7 @@ const program = new Command()
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .version('0.1.0')
+  .version('1.0.0')
   .argument('<filepath1>')
   .argument('<filepath2>')
   .option('-f, --format [type]', 'output format', 'stylish')
